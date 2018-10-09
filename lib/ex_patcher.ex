ececf0eed9eb3fef92d8fbf72d595a381d106308
Arjan Scherpenisse <arjan@scherpenisse.net>
2018-10-09 21:18:44 +0200

    Filter_map

--- lib/ex_patcher.ex
+++ lib/ex_patcher.ex
@@ -6,15 +6,26 @@ defmodule ExPatcher do
   end
 
   def patch_io_chardata_to_string({file, ast}) do
-    ast = Macro.postwalk(ast, &fix_chardata_to_string/1)
+    ast = Macro.postwalk(ast, &fix_it/1)
     {file, ast}
   end
 
-  defp fix_chardata_to_string({:., meta, [{:__aliases__, b, [:IO]}, :char_data_to_string]}) do
+  defp fix_it({:., meta, [{:__aliases__, b, [:IO]}, :char_data_to_string]}) do
     {:., meta, [{:__aliases__, b, [:IO]}, :chardata_to_string]}
   end
 
-  defp fix_chardata_to_string(ast) do
+  defp fix_it({:greet, meta, [a, b]}) do
+    {:greet, meta, [b, a]}
+  end
+
+  defp fix_it({{:., a, [{:__aliases__, b, [:Enum]}, :filter_map]}, c, [filter, mapper]}) do
+    quote do
+      Enum.filter(unquote(filter))
+      |> Enum.map(unquote(mapper))
+    end
+  end
+
+  defp fix_it(ast) do
     ast
   end
 
